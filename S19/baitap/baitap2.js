c5a2d647a8bc87f367876844e9d327e9020ffdc4
duonvipbn <duonvipbn@gmail.com>
2025-03-03 23:14:09 +0700

    Update baitap2.js

--- S19/baitap/baitap2.js
+++ S19/baitap/baitap2.js
@@ -1,5 +1,5 @@
 let input = prompt("nhập:");
-if ((input.length === 1) && (typeof input === "string") && (input.toLowerCase() !== input.toUpperCase())) {
+if ((input.length === 1) && (isNaN(input)) && (input.toLowerCase() !== input.toUpperCase())) {
     alert(`Ký tự ${input} là chữ cái`);
 } else {
     alert(`${input} là không là chữ cái`);
